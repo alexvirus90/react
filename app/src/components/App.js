228877440bef76cd17c837722e37ea8f8ec09767
Komarov Alexander <a.kom90@mail.ru>
2018-04-28 12:24:59 +0300

    created the project by react, first project

---
+++ app/src/components/App.js
@@ -0,0 +1,33 @@
+import React, {Component, PureComponent} from 'react'
+import ArticleList from './ArticleList'
+import articles from '../articles'
+import 'bootstrap/dist/css/bootstrap.css'
+// import articles from '../fixtures.js'
+
+class App extends PureComponent {
+	state = {
+		reverted: false
+	};
+
+	render () {
+		return (
+			<div className="container">
+				<div className="jumbotron">
+					<h1 className="display-3">
+						App name
+						<button className="btn" onClick={this.revert}>Revert</button>
+					</h1>
+				</div>
+				<ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>
+			</div>
+		)
+	}
+
+	revert = () => {
+		this.setState({
+			reverted: !this.state.reverted
+		})
+	}
+}
+
+export default App
